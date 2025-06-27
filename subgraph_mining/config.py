569f91cb9e02bebb681157319ce7edd703a0428e
abel <ashenafiabel49@gmail.com>
2025-06-27 10:59:35 +0300

    run

--- subgraph_mining/config.py
+++ subgraph_mining/config.py
@@ -59,7 +59,7 @@ def parse_decoder(parser):
         
         # Decoder defaults
         out_path="results/out-patterns.p",
-        n_neighborhoods=10000,
+        n_neighborhoods=1000,
         n_trials=100,
         decode_thresh=0.5,
         radius=3,
